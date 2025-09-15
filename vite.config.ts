4ce9cdc19352caaafa6541c6a79f949f043e9f06
Inkyu Oh <gomjellie@gmail.com>
2025-09-15 16:43:30 +0900

    :bento: robots.txt 추가 및 github pages에 sitemap, manifest.json 등이 올라가도록 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
 const manifestForPlugin: Partial<VitePWAOptions> = {
   registerType: 'prompt',
-  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable_icon.png'],
+  includeAssets: [
+    'favicon.ico',
+    'apple-touch-icon.png',
+    'maskable_icon.png',
+    'robots.txt',
+    'sitemap.xml',
+    'manifest.json',
+  ],
   manifest: {
     name: 'Skull King',
     short_name: 'Skull King',
@@ -13,25 +20,25 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
       {
         src: 'favicon.ico',
         sizes: '64x64 32x32 24x24 16x16',
-        type: 'image/x-icon'
+        type: 'image/x-icon',
       },
       {
         src: 'logo192.png',
         type: 'image/png',
-        sizes: '192x192'
+        sizes: '192x192',
       },
       {
         src: 'logo512.png',
         type: 'image/png',
-        sizes: '512x512'
-      }
+        sizes: '512x512',
+      },
     ],
     start_url: '.',
     display: 'standalone',
     theme_color: '#000000',
-    background_color: '#ffffff'
-  }
-}
+    background_color: '#ffffff',
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -39,7 +46,7 @@ export default defineConfig({
   plugins: [react(), VitePWA(manifestForPlugin)],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
